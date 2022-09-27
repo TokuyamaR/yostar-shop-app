e9ba3fdd724e86ec5f14c6285221dbc3d3e868de
TokuyamaR <qpflapper.1007@gmail.com>
2022-09-27 22:27:03 +0900

    style: tailwindcssのスタイルを追加

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,15 @@
 import React from "react";
 import App from "next/app";
 import Head from "next/head";
+import "../styles/globals.css";
 
 export default class MyApp extends App {
   render() {
-    const { Componet, pageProps } = this.props;
+    const { Component, pageProps } = this.props;
     return (
       <>
         <Head></Head>
-        <Comonent {...pageProps} />
+        <Component {...pageProps} />
       </>
     );
   }
