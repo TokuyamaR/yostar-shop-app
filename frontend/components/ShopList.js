6075bd098f7eae69dc23d3f2751f7e3d085b724b
TokuyamaR <qpflapper.1007@gmail.com>
2022-10-01 12:18:27 +0900

    feat: GraphQLによるshopデータの取得クエリを追加

--- frontend/components/ShopList.js
+++ frontend/components/ShopList.js
@@ -1,29 +1,29 @@
 import { Card } from "./Card";
-// import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 
-// const query = gql`
-//   {
-//     yostarShops {
-//       data {
-//         id
-//         attributes {
-//           name
-//           description
-//           image {
-//             data {
-//               attributes {
-//                 url
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+const query = gql`
+  {
+    yostarShops {
+      data {
+        id
+        attributes {
+          name
+          description
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const ShopList = () => {
-  // const { loading, error, data } = useQuery(query);
+  const { loading, error, data } = useQuery(query);
   return (
     <div className="grid grid-cols-3">
       <Card />
