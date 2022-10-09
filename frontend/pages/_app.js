382bf27da82067bdc377117612371b96c762ccbd
TokuyamaR <qpflapper.1007@gmail.com>
2022-10-09 12:25:25 +0900

    chore: 登録するユーザー情報をcontextとして登録する処理ori

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
 import "../styles/globals.css";
 import createApolloClient from "../lib/apolloClient";
 import { ApolloProvider } from "@apollo/client";
+import AppContext from "../context/AppContext";
 
 // ====== next/app を利用したpageの表示 ======
 // import App from "next/app";
@@ -22,13 +24,16 @@ import { ApolloProvider } from "@apollo/client";
 
 // ====== apolloを利用したpageの表示 ======
 export default function App({ Component, pageProps }) {
+  const [user, setUser] = useState(null);
   const client = createApolloClient();
   return (
-    <ApolloProvider client={client}>
-      <Head></Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <AppContext.Provider value={{ user, setUser }}>
+      <ApolloProvider client={client}>
+        <Head></Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </AppContext.Provider>
   );
 }
