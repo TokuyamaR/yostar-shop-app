2ab97e0e682ea52f1643f9caddc9b1473364da88
TokuyamaR <qpflapper.1007@gmail.com>
2022-10-11 23:58:19 +0900

    fix: 非同期処理の結果が正常に受け取れていない不具合

--- frontend/lib/auth.js
+++ frontend/lib/auth.js
@@ -2,39 +2,43 @@ import axios from "axios";
 import { API_URL } from "../config";
 import Cookies from "js-cookie";
 
-export const registerUser = async (username, email, password) => {
-  await axios
-    .post(`${API_URL}/api/auth/local/register`, {
-      username,
-      email,
-      password,
-    })
-    .then((response) => {
-      console.log("POST成功");
-      console.log("登録内容：", response.data.user);
-      console.log("トークン：", response.data.jwt);
-      Cookies.set("token", response.data.jwt, { expires: 7 });
-      window.location.href = "/";
-    })
-    .catch((error) => {
-      console.log("エラー発生：", error.response);
-    });
+export const registerUser = (username, email, password) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${API_URL}/api/auth/local/register`, {
+        username,
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log("POST成功");
+        Cookies.set("token", response.data.jwt, { expires: 7 });
+        resolve(response);
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        reject(error);
+        console.log("エラー発生：", error.response);
+      });
+  });
 };
 
-export const login = async (identifier, password) => {
-  await axios
-    .post(`${API_URL}/api/auth/local`, {
-      identifier,
-      password,
-    })
-    .then((response) => {
-      console.log("POST成功");
-      console.log("登録内容：", response.data.user);
-      console.log("トークン：", response.data.jwt);
-      Cookies.set("token", response.data.jwt, { expires: 7 });
-      window.location.href = "/";
-    })
-    .catch((error) => {
-      console.log("エラー発生：", error.response);
-    });
+export const login = (identifier, password) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${API_URL}/api/auth/local`, {
+        identifier,
+        password,
+      })
+      .then((response) => {
+        console.log("POST成功");
+        Cookies.set("token", response.data.jwt, { expires: 7 });
+        resolve(response);
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        reject(error);
+        console.log("エラー発生：", error.response);
+      });
+  });
 };
