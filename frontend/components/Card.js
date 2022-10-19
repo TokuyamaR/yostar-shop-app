b6e6772d633c7142234228630408157859526367
TokuyamaR <qpflapper.1007@gmail.com>
2022-10-19 23:58:34 +0900

    feat: Cardコンポーネントのpropsを変更

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -2,11 +2,13 @@
 import { API_URL } from "../config";
 import RightArrow from "../public/images/arrow-right.svg";
 import Link from "next/link";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
-export const Card = ({ data, linkText, path }) => {
-  const id = data.id;
-  const { name, description, image } = data.attributes;
+export const Card = ({ data, actionType = "link", text, path }) => {
+  const { id, name, description, image } = data;
   const imageSrc = `${API_URL}${image.data.attributes.url}`;
+  const { addItem } = useContext(AppContext);
 
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -31,11 +33,20 @@ export const Card = ({ data, linkText, path }) => {
           {description}
         </p>
         <div className="text-right">
-          <Link href={`${path}/?id=${id}`} as={`${path}/${id}`}>
-            <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              {linkText} <RightArrow className="w-4 h-4 ml-2 -mr-1" />
-            </a>
-          </Link>
+          {actionType !== "link" ? (
+            <button
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => addItem(data)}
+            >
+              {text} <RightArrow className="w-4 h-4 ml-2 -mr-1" />
+            </button>
+          ) : (
+            <Link href={`${path}/?id=${id}`} as={`${path}/${id}`}>
+              <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                {text} <RightArrow className="w-4 h-4 ml-2 -mr-1" />
+              </a>
+            </Link>
+          )}
         </div>
       </div>
     </div>
