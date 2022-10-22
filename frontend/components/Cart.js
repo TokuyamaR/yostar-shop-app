544e79ad5e097a3222543b42e8220ce63977c1cc
TokuyamaR <qpflapper.1007@gmail.com>
2022-10-22 12:20:34 +0900

    style: カート内アイテムの有無で表示とスタイルを出し分ける

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,39 +1,58 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../context/AppContext";
 
 export const Cart = () => {
   const { cart } = useContext(AppContext);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    cart.items.length > 0 ? setIsDisabled(false) : setIsDisabled(true);
+  }, [cart.items.length]);
+
   return (
     <div className="bg-white border border-gray-200 rounded-lg shadow-md w-80 h-fit">
       <h3 className="p-4 text-xl font-bold border-b border-gray-200">
         注文一覧
       </h3>
-      <div className="p-4 space-y-4 border-b border-gray-200">
-        {cart.items.map((item) => (
-          <div key={item.id} className="space-y-4">
-            <div className="space-x-2">
-              <span>{item.name}</span>
-              <span className="font-bold">¥&nbsp;{item.price}</span>
+      {cart.items ? (
+        <>
+          <div className="p-4 space-y-4 border-b border-gray-200">
+            {cart.items.map((item) => {
+              if (item.quantity > 0) {
+                return (
+                  <div key={item.id} className="space-y-4">
+                    <div className="space-x-2">
+                      <span>{item.name}</span>
+                      <span className="font-bold">¥&nbsp;{item.price}</span>
+                    </div>
+                    <div className="space-x-2">
+                      <button className="px-2 bg-gray-200 rounded">+</button>
+                      <button className="px-2 bg-gray-200 rounded">-</button>
+                      <span>数量：{item.quantity}</span>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="p-4 space-y-4">
+            <div className="space-x-2 font-bold">
+              <span>合計</span>
+              <span>{`${cart.totalPrice}円`}</span>
             </div>
-            <div className="space-x-2">
-              <button className="px-2 bg-gray-200 rounded">+</button>
-              <button className="px-2 bg-gray-200 rounded">-</button>
-              <span>数量：{item.quantity}</span>
+            <div className="text-right">
+              <button
+                disable={isDisabled}
+                className={`px-4 py-2 text-white bg-blue-600 rounded hover:opacity-80 ${
+                  isDisabled ? "bg-gray-200" : ""
+                }`}
+              >
+                注文する
+              </button>
             </div>
           </div>
-        ))}
-      </div>
-      <div className="p-4 space-y-4">
-        <div className="space-x-2 font-bold">
-          <span>合計</span>
-          <span>{`${cart.totalPrice}円`}</span>
-        </div>
-        <div className="text-right">
-          <button className="px-4 py-2 text-white bg-blue-600 rounded hover:opacity-80">
-            注文する
-          </button>
-        </div>
-      </div>
+        </>
+      ) : null}
     </div>
   );
 };
