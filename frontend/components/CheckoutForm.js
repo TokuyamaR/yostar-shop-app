5eac9d5eba08dc6d294988d1773496b6fcd6c518
TokuyamaR <qpflapper.1007@gmail.com>
2022-10-30 11:20:30 +0900

    feat: 決済用フォームコンポーネントを適用

--- frontend/components/CheckoutForm.js
+++ frontend/components/CheckoutForm.js
@@ -1,5 +1,36 @@
-const CheckoutForm = () => {
-  return <div>Checkout Form Component</div>;
-};
+import { CreditPaymentSection } from "./CreditPaymentSection";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { STRIPE_PUBLISHABLE_KEY } from "../config";
 
-export default CheckoutForm;
+const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
+export const CheckoutForm = () => {
+  return (
+    <div className="bg-white border border-gray-200 rounded-lg shadow-md w-80 h-fit">
+      <h3 className="p-4 text-lg border-b border-gray-200">決済情報</h3>
+      <div className="p-4 space-y-4 border-b border-gray-200">
+        <form action="post" className="space-y-4">
+          <label>
+            <p className="text-gray-500">住所</p>
+            <input
+              type="text"
+              className="w-full p-1 border border-gray-300 rounded"
+              placeholder="東京都港区六本木３丁目２−１"
+            />
+          </label>
+          <Elements stripe={stripePromise}>
+            <CreditPaymentSection />
+          </Elements>
+          <div className="text-right">
+            <button
+              type="submit"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-400 rounded hover:bg-red-300 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              注文を確認
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
--- frontend/pages/checkout.js
+++ frontend/pages/checkout.js
@@ -1,12 +1,15 @@
 import { Cart } from "../components/Cart";
+import { CheckoutForm } from "../components/CheckoutForm";
 
 const checkout = (props) => {
   return (
     <div className="space-y-4">
       <h1 className="text-3xl font-bold">注文情報の確認</h1>
       <div className="flex">
-        <div className="grid grid-cols-3 gap-4"></div>
         <Cart />
+        <div>
+          <CheckoutForm />
+        </div>
       </div>
     </div>
   );
